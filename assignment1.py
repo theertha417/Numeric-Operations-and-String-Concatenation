e0036ae2affc78d9005d840e891bc0172d74fa60
theertha417 <theerthachinthulal@gmail.com>
2026-03-27 14:09:26 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,28 @@
+# Numeric Operations 
+a = 14
+print("A = ",a)
+b = 6
+print("B = ",b)
+#Addition
+print("A+B = ",a+b)
+#Subtraction 
+print("A-B = ",a-b)
+#Multplication
+print("A*B = ",a*b)
+#Division
+print("A/B = ",a/b)
+#Floor division
+print("A//B = ",a//b)
+#Exponent
+print("A**b = ",a**b)
+#modulus
+print("A%B = ",a%b)
+#String Concatenation
+a = "Hello"
+b = "Theertha"
+print(a+b)
+
+age= 27
+print("age is"+str(age))
+
+
